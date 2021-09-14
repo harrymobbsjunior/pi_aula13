a2db18beeba739f443552bd091e805078515ef15
Harry Möbbs Júnior <harrymobbsjunior@gmail.com>
2021-09-13 22:08:10 -0300

    Funções revisadas e funcionando

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ function cliente(conta, tipoConta, saldo, nome) {
     this.nome = nome;
 }
 
-let cliente1 = new cliente(5486273622, 'Conta Corrente', '27771', 'Abigael Natte');
-let cliente2 = new cliente(1183971869, 'Conta Poupança', '8675', 'Ramon Connell');
-let cliente3 = new cliente(9582019689, 'Conta Poupança', '27363', 'Jarret Lafuente');
-let cliente4 = new cliente(1761924656, 'Conta Poupança', '32415', 'Ansel Ardley');
-let cliente5 = new cliente(7401971607, 'Conta Poupança', '18789', 'Jacki Shurmer');
-let cliente6 = new cliente(630841470, 'Conta Corrente', '28776', 'Jobi Mawtus');
-let cliente7 = new cliente(4223508636, 'Conta Corrente', '2177', 'Thomasin Latour');
-let cliente8 = new cliente(185979521, 'Conta Poupança', '25994', 'Lonnie Verheijden');
-let cliente9 = new cliente(3151956165, 'Conta Corrente', '7601', 'Alonso Wannan');
-let cliente10 = new cliente(2138105881, 'Conta Poupança', '33196', 'Bendite Huggett');
+let cliente1 = new cliente(5486273622, 'Conta Corrente', 277.71, 'Abigael Natte');
+let cliente2 = new cliente(1183971869, 'Conta Poupança', 867.5, 'Ramon Connell');
+let cliente3 = new cliente(9582019689, 'Conta Poupança', 273.63, 'Jarret Lafuente');
+let cliente4 = new cliente(1761924656, 'Conta Poupança', 324.15, 'Ansel Ardley');
+let cliente5 = new cliente(7401971607, 'Conta Poupança', 187.89, 'Jacki Shurmer');
+let cliente6 = new cliente(630841470, 'Conta Corrente', 287.76, 'Jobi Mawtus');
+let cliente7 = new cliente(4223508636, 'Conta Corrente', 217.7, 'Thomasin Latour');
+let cliente8 = new cliente(185979521, 'Conta Poupança', 259.94, 'Lonnie Verheijden');
+let cliente9 = new cliente(3151956165, 'Conta Corrente', 760.1, 'Alonso Wannan');
+let cliente10 = new cliente(2138105881, 'Conta Poupança', 331.96, 'Bendite Huggett');
 
 //console.log(cliente1);
 
@@ -34,17 +34,15 @@ lista2.push(cliente10);
 
 //console.log(lista2);
 
-
 let banco = {
     listaClientes: lista,
     consultarCliente: function (nome) {
-        for (var i = 0; i < this.listaClientes.lenght; i++) {
+        //console.log(this.listaClientes.length);
+        for (var i = 0; i < this.listaClientes.length; i++) {
             const cliente = this.listaClientes[i];;
             if (cliente.nome == nome) {
                 console.log(cliente);
                 return cliente;
-            }else {
-                console.log("erro");
             }
         }
     },
@@ -60,15 +58,14 @@ let banco = {
         }else {
             cliente.saldo -= valor;
         }
-        // cliente.saldo += valor;
-        // console.log("Depósito realizado, seu novo saldo é: " + cliente.saldo);
+        console.log("Saque realizado, seu novo saldo é: " + cliente.saldo);
     }
 }
 
-//console.log(banco);
-banco.consultarCliente("Bendite Huggett");
-//banco.deposito("Bendite Huggett", 50.10);
-//banco.saque("Bendite Huggett", 50);
+//console.log(banco.listaClientes);
+//banco.consultarCliente("Bendite Huggett");
+//banco.deposito("Bendite Huggett", 150);
+//banco.saque("Bendite Huggett", 250);
 
 
 
